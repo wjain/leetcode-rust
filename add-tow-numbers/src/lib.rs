cbe6f32eb3e045e38ed62034b2e34bae116cdd00
wjain <jain.y@126.com>
2021-05-19 00:22:59 +0800

    add two numbers# 请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交# 说明将会终止提交。## 位于分支 main# 您的分支与上游分支 'origin/main' 一致。## 要提交的变更：#	修改：     add-tow-numbers/src/lib.rs## 未跟踪的文件:#	add-tow-numbers/target/#

--- add-tow-numbers/src/lib.rs
+++ add-tow-numbers/src/lib.rs
@@ -1,29 +1,57 @@
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
 }
 
 impl ListNode {
-  #[inline]
-  fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
     }
-  }
 }
 
 pub struct Solution;
 
 impl Solution {
-    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    pub fn add_two_numbers(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let mut dummy1 = l1;
+        let mut dummy2 = l2;
+
+        let mut carry = 0;
+
         let mut root = Some(Box::new(ListNode::new(0)));
+        let mut curr = &mut root;
 
+        while dummy1.is_some() || dummy1.is_some() {
+            match curr {
+                Some(inner_node) => {
+                    let first = dummy1.take().unwrap_or(Box::new(ListNode::new(0)));
+                    let second = dummy2.take().unwrap_or(Box::new(ListNode::new(0)));
 
-        while l1.is_some() || l2.is_some() {
-            
+                    let mut sum = first.val + second.val + carry;
+
+                    carry = sum / 10;
+                    sum = sum % 10;
+
+                    inner_node.next.get_or_insert(Box::new(ListNode::new(sum)));
+
+                    curr = &mut inner_node.next;
+                    dummy1 = first.next;
+                    dummy2 = second.next;
+                }
+                None => break,
+            }
+        }
+
+        if carry == 1 {
+            if let Some(node) = curr {
+                node.next.get_or_insert(Box::new(ListNode::new(1)));
+            }
         }
 
         root.unwrap().next
@@ -32,8 +60,38 @@ impl Solution {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let l1 = Some(Box::new(ListNode {
+            val: 9,
+            next: Some(Box::new(ListNode {
+                val: 8,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode::new(3))),
+                })),
+            })),
+        }));
+        let l2 = Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode::new(3))),
+            })),
+        }));
+        assert_eq!(
+            Some(Box::new(ListNode {
+                val: 0,
+                next: Some(Box::new(ListNode {
+                    val: 1,
+                    next: Some(Box::new(ListNode {
+                        val: 7,
+                        next: Some(Box::new(ListNode::new(3))),
+                    })),
+                })),
+            })),
+            Solution::add_two_numbers(l1, l2)
+        );
     }
 }
