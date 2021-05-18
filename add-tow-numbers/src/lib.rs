1b2edca2cbadce5f0cc983d7f7ad4dca0cb458d8
jain.y <jain_y@126.com>
2021-05-18 21:48:14 +0800

    add tow numbers init

---
+++ add-tow-numbers/src/lib.rs
@@ -0,0 +1,39 @@
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+
+pub struct Solution;
+
+impl Solution {
+    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut root = Some(Box::new(ListNode::new(0)));
+
+
+        while l1.is_some() || l2.is_some() {
+            
+        }
+
+        root.unwrap().next
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
